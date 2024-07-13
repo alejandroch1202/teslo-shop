d5df6dba8f27c81820ccb216b273078198fe8f13
alejandroch1202 <alejandrochavez1202@gmail.com>
2024-07-12 20:00:26 -0400

    Moving frontend to the root

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 <p align="center">
-  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="150" alt="Nest Logo" /></a>
   <a href="https://nextjs.org" target="blank"><img src="https://assets.vercel.com/image/upload/v1662130559/nextjs/Icon_light_background.png" alt="Next Logo" width="150" /></a>
 </p>
 
 # Teslo Shop
+
+1. Clone the repository
+
+2. Create a copy of the `.env.example` file and name it `.env`, then change the environment variables.
+
+3. Install dependencies
+
+```
+yarn
+```
+
+4.  Start the development database
+
+```
+docker-compose up -d
+```
+
+5. Run the project in development mode
+
+```
+yarn dev
+```
--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -15,9 +15,9 @@ export default function CartPage() {
   // redirect('/empty')
 
   return (
-    <div className='flex justify-center items-center mb-72 px019 sm:px-0'>
+    <div className='flex justify-center items-center mb-72 px-10 sm:px-0'>
       <div className='flex flex-col w-[1000px]'>
-        <Title title='Cart' />
+        <Title title='Carrito' />
 
         <div className='grid grid-cols-1 sm:grid-cols-2 gap-10'>
           {/* cart */}
@@ -67,7 +67,7 @@ export default function CartPage() {
           </div>
 
           {/* order summary */}
-          <div className='bg-white rounded-xl shadow-lg p-7'>
+          <div className='bg-white rounded-xl shadow-lg p-7 h-fit'>
             <h2 className='text-2xl font-bold mb-5'>Resumen de la orden</h2>
 
             <div className='flex justify-between mb-5'>
